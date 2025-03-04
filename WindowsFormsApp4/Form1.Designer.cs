2263d04a7558b2f208946e6b91006520bde93116
gmartinog <gmartinog@PW06S479>
2025-03-04 14:13:06 -0500

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBoxRaw;
+        private System.Windows.Forms.PictureBox pictureBoxLaneLines;
+        private System.Windows.Forms.PictureBox pictureBoxRedLine;
+        private System.Windows.Forms.PictureBox pictureBoxEdges;
+        private System.Windows.Forms.PictureBox pictureBoxBlurred;
+        private System.Windows.Forms.Label directionLabel;
+
+        private System.Windows.Forms.TrackBar laneThresholdBar;
+        private System.Windows.Forms.TrackBar redThresholdBar;
+        private System.Windows.Forms.TrackBar edgeThresholdBar;
+        private System.Windows.Forms.TrackBar blurKernelBar;
+
+        private System.Windows.Forms.Label laneThresholdLabel;
+        private System.Windows.Forms.Label redThresholdLabel;
+        private System.Windows.Forms.Label edgeThresholdLabel;
+        private System.Windows.Forms.Label blurKernelLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBoxRaw = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLaneLines = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRedLine = new System.Windows.Forms.PictureBox();
+            this.pictureBoxEdges = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlurred = new System.Windows.Forms.PictureBox();
+            this.directionLabel = new System.Windows.Forms.Label();
+
+            this.laneThresholdBar = new System.Windows.Forms.TrackBar();
+            this.redThresholdBar = new System.Windows.Forms.TrackBar();
+            this.edgeThresholdBar = new System.Windows.Forms.TrackBar();
+            this.blurKernelBar = new System.Windows.Forms.TrackBar();
+
+            this.laneThresholdLabel = new System.Windows.Forms.Label();
+            this.redThresholdLabel = new System.Windows.Forms.Label();
+            this.edgeThresholdLabel = new System.Windows.Forms.Label();
+            this.blurKernelLabel = new System.Windows.Forms.Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLaneLines)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRedLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEdges)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlurred)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.laneThresholdBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redThresholdBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edgeThresholdBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blurKernelBar)).BeginInit();
+            this.SuspendLayout();
+
+            // PictureBoxes
+            this.pictureBoxRaw.Location = new System.Drawing.Point(20, 20);
+            this.pictureBoxRaw.Size = new System.Drawing.Size(300, 200);
+            this.pictureBoxRaw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+
+            this.pictureBoxLaneLines.Location = new System.Drawing.Point(340, 20);
+            this.pictureBoxLaneLines.Size = new System.Drawing.Size(300, 200);
+            this.pictureBoxLaneLines.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+
+            this.pictureBoxRedLine.Location = new System.Drawing.Point(660, 20);
+            this.pictureBoxRedLine.Size = new System.Drawing.Size(300, 200);
+            this.pictureBoxRedLine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+
+            this.pictureBoxEdges.Location = new System.Drawing.Point(20, 250);
+            this.pictureBoxEdges.Size = new System.Drawing.Size(300, 200);
+            this.pictureBoxEdges.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+
+            this.pictureBoxBlurred.Location = new System.Drawing.Point(340, 250);
+            this.pictureBoxBlurred.Size = new System.Drawing.Size(300, 200);
+            this.pictureBoxBlurred.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+
+            // Labels
+            this.directionLabel.Location = new System.Drawing.Point(660, 260);
+            this.directionLabel.Size = new System.Drawing.Size(200, 30);
+            this.directionLabel.Text = "Direction: Unknown";
+
+            // TrackBars for filter adjustments
+            this.laneThresholdBar.Location = new System.Drawing.Point(20, 500);
+            this.laneThresholdBar.Minimum = 50;
+            this.laneThresholdBar.Maximum = 255;
+            this.laneThresholdBar.Value = 200;
+            this.laneThresholdBar.TickFrequency = 10;
+
+            this.redThresholdBar.Location = new System.Drawing.Point(340, 500);
+            this.redThresholdBar.Minimum = 50;
+            this.redThresholdBar.Maximum = 255;
+            this.redThresholdBar.Value = 100;
+            this.redThresholdBar.TickFrequency = 10;
+
+            this.edgeThresholdBar.Location = new System.Drawing.Point(660, 500);
+            this.edgeThresholdBar.Minimum = 50;
+            this.edgeThresholdBar.Maximum = 255;
+            this.edgeThresholdBar.Value = 100;
+            this.edgeThresholdBar.TickFrequency = 10;
+
+            this.blurKernelBar.Location = new System.Drawing.Point(20, 550);
+            this.blurKernelBar.Minimum = 1;
+            this.blurKernelBar.Maximum = 15;
+            this.blurKernelBar.Value = 9;
+            this.blurKernelBar.SmallChange = 2;
+            this.blurKernelBar.LargeChange = 2;
+            this.blurKernelBar.TickFrequency = 2;
+
+            // Labels for TrackBars
+            this.laneThresholdLabel.Location = new System.Drawing.Point(20, 530);
+            this.laneThresholdLabel.Size = new System.Drawing.Size(200, 20);
+            this.laneThresholdLabel.Text = "Lane Threshold: 200";
+
+            this.redThresholdLabel.Location = new System.Drawing.Point(340, 530);
+            this.redThresholdLabel.Size = new System.Drawing.Size(200, 20);
+            this.redThresholdLabel.Text = "Red Threshold: 100";
+
+            this.edgeThresholdLabel.Location = new System.Drawing.Point(660, 530);
+            this.edgeThresholdLabel.Size = new System.Drawing.Size(200, 20);
+            this.edgeThresholdLabel.Text = "Edge Threshold: 100";
+
+            this.blurKernelLabel.Location = new System.Drawing.Point(20, 580);
+            this.blurKernelLabel.Size = new System.Drawing.Size(200, 20);
+            this.blurKernelLabel.Text = "Blur Kernel: 9";
+
+            // Adding Controls to Form
+            this.Controls.Add(this.pictureBoxRaw);
+            this.Controls.Add(this.pictureBoxLaneLines);
+            this.Controls.Add(this.pictureBoxRedLine);
+            this.Controls.Add(this.pictureBoxEdges);
+            this.Controls.Add(this.pictureBoxBlurred);
+            this.Controls.Add(this.directionLabel);
+
+            this.Controls.Add(this.laneThresholdBar);
+            this.Controls.Add(this.redThresholdBar);
+            this.Controls.Add(this.edgeThresholdBar);
+            this.Controls.Add(this.blurKernelBar);
+
+            this.Controls.Add(this.laneThresholdLabel);
+            this.Controls.Add(this.redThresholdLabel);
+            this.Controls.Add(this.edgeThresholdLabel);
+            this.Controls.Add(this.blurKernelLabel);
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLaneLines)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRedLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEdges)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlurred)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.laneThresholdBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redThresholdBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edgeThresholdBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blurKernelBar)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Timer _frameTimer;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            
+            if (laneThresholdBar != null) laneThresholdBar.Scroll += FilterUpdated;
+            if (redThresholdBar != null) redThresholdBar.Scroll += FilterUpdated;
+            if (edgeThresholdBar != null) edgeThresholdBar.Scroll += FilterUpdated;
+            if (blurKernelBar != null) blurKernelBar.Scroll += FilterUpdated;
+
+           
+
+            _capture = new VideoCapture(0);
+            
+
+            _frameTimer = new Timer { Interval = 100 };
+            _frameTimer.Tick += FrameTimer_Tick;
+            _frameTimer.Start();
+            
+
+            
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+        }
+
+        private void FrameTimer_Tick(object sender, EventArgs e)
+        {
+            Mat frame = _capture.QueryFrame();
+            if (frame != null && !frame.IsEmpty)
+            {
+                CvInvoke.Flip(frame, frame, FlipType.Horizontal);
+                DisplayImage(frame, pictureBoxRaw);
+                ProcessFilters(frame);
+            }
+        }
+
+        private void DisplayImage(Mat image, PictureBox pictureBox)
+        {
+            Image<Bgr, Byte> resizedImage = image.ToImage<Bgr, Byte>()
+                .Resize(pictureBox.Width, pictureBox.Height, Inter.Linear);
+
+            pictureBox.Image?.Dispose();
+            pictureBox.Image = resizedImage.ToBitmap();
+        }
+
+        private void ProcessFilters(Mat frame)
+        {
+            Image<Gray, Byte> laneLines = DetectLaneLines(frame);
+            Image<Gray, Byte> redLine = DetectRedLine(frame);
+            Image<Gray, Byte> edges = DetectEdges(frame);
+            Image<Bgr, Byte> blurred = ApplyBlur(frame);
+
+            pictureBoxLaneLines.Image?.Dispose();
+            pictureBoxLaneLines.Image = laneLines.ToBitmap();
+
+            pictureBoxRedLine.Image?.Dispose();
+            pictureBoxRedLine.Image = redLine.ToBitmap();
+
+            pictureBoxEdges.Image?.Dispose();
+            pictureBoxEdges.Image = edges.ToBitmap();
+
+            pictureBoxBlurred.Image?.Dispose();
+            pictureBoxBlurred.Image = blurred.ToBitmap();
+
+            AnalyzeLanePosition(laneLines, redLine);
+        }
+
+        private Image<Gray, Byte> DetectLaneLines(Mat frame)
+        {
+            laneThresholdLabel.Text = $"Lane Threshold: {laneThresholdBar.Value}";
+            return frame.ToImage<Bgr, Byte>().Convert<Hsv, Byte>()
+                        .InRange(new Hsv(0, 0, laneThresholdBar.Value), new Hsv(180, 50, 255));
+        }
+
+        private Image<Gray, Byte> DetectRedLine(Mat frame)
+        {
+            redThresholdLabel.Text = $"Red Threshold: {redThresholdBar.Value}";
+            return frame.ToImage<Bgr, Byte>().Convert<Hsv, Byte>()
+                        .InRange(new Hsv(0, redThresholdBar.Value, 100), new Hsv(10, 255, 255));
+        }
+
+        private Image<Gray, Byte> DetectEdges(Mat frame)
+        {
+            edgeThresholdLabel.Text = $"Edge Threshold: {edgeThresholdBar.Value}";
+            Mat edges = new Mat();
+            CvInvoke.Canny(frame, edges, edgeThresholdBar.Value, edgeThresholdBar.Value * 2);
+            return edges.ToImage<Gray, Byte>();
+        }
+
+        private Image<Bgr, Byte> ApplyBlur(Mat frame)
+        {
+            blurKernelLabel.Text = $"Blur Kernel: {blurKernelBar.Value}";
+            Mat blurred = new Mat();
+            CvInvoke.GaussianBlur(frame, blurred, new Size(blurKernelBar.Value, blurKernelBar.Value), 0);
+            return blurred.ToImage<Bgr, Byte>();
+        }
+
+        private void AnalyzeLanePosition(Image<Gray, Byte> laneImage, Image<Gray, Byte> redLineImage)
+        {
+            int width = laneImage.Width;
+            int sliceWidth = width / 5;
+            int[] whiteCounts = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Rectangle sliceRect = new Rectangle(i * sliceWidth, 0, sliceWidth, laneImage.Height);
+                Image<Gray, Byte> slice = laneImage.GetSubRect(sliceRect);
+
+                whiteCounts[i] = CvInvoke.CountNonZero(slice);
+            }
+
+            DetermineDirection(whiteCounts, redLineImage);
+        }
+
+        private void DetermineDirection(int[] whiteCounts, Image<Gray, Byte> redLineImage)
+        {
+            if (CvInvoke.CountNonZero(redLineImage) > 500)
+            {
+                UpdateDirectionLabel("stop");
+                return;
+            }
+
+            int maxIndex = Array.IndexOf(whiteCounts, whiteCounts.Max());
+
+            string direction;
+            switch (maxIndex)
+            {
+                case 0:
+                    direction = "Left Left";
+                    break;
+                case 1:
+                    direction = "Left";
+                    break;
+                case 2:
+                    direction = "Middle";
+                    break;
+                case 3:
+                    direction = "Right";
+                    break;
+                case 4:
+                    direction = "Right Right";
+                    break;
+                default:
+                    direction = "Unknown";
+                    break;
+            }
+
+            UpdateDirectionLabel(direction);
+        }
+
+        private void UpdateDirectionLabel(string direction)
+        {
+            this.Invoke(new Action(() =>
+            {
+                directionLabel.Text = $"Direction: {direction}";
+            }));
+        }
+
+        private void FilterUpdated(object sender, EventArgs e) => ProcessFilters(_capture.QueryFrame());
+    }
+}
